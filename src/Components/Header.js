3d7f311b5dd355a6dbe7c16a42e111511520d30b
sameer-shaikh <sameermubeen4@gmail.com>
2021-01-31 22:21:35 +0500

    Added New Updates On scrollBar and Header Image

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import coronaLogo from '../Images/coronaLogo.png'
+import React from "react";
+import coronaLogo from "../Images/coronaLogo.png";
 
 const Header = () => {
+	const image = coronaLogo;
 
-    const image = coronaLogo;
+	return (
+		<div>
+			<h1 className="main_header">
+				C<img className="Header_image" src={image} alt="Covid" />
+				vid Tracker App
+			</h1>
+		</div>
+	);
+};
 
-    return (
-        <div>
-            <h1 className="main_header"> C<img src={image} alt="Covid" />vid Tracker App </h1>
-        </div>
-    )
-}
-
-export default Header
+export default Header;
